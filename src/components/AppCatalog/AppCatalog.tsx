b5b83be8b1dfaf42f434c28a08a1b513aaa57d3e
Ilya Dragonromeo <you@example.com>
2024-02-20 14:29:27 +0300

    refactor: remove commented code

--- src/components/AppCatalog/AppCatalog.tsx
+++ src/components/AppCatalog/AppCatalog.tsx
@@ -1,11 +1,8 @@
 import { Catalog } from "./Catalog/Catalog";
 import { Filters } from "./Filters/Filters";
 import style from "./AppCatalog.module.scss";
-// import { useState } from "react";
 
 const AppCatalog = () => {
-  // const [loading, setLoading] = useState(true);
-
   return (
     <div className={style.app_catalog}>
       <Filters />
